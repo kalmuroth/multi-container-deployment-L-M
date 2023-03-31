5555f5aaff7e438db2886b3ebf8a6b40ea14e081
kalmuroth <dokkan.dracktar@gmail.com>
2023-03-31 15:13:36 +0200

    fixes pdo conn

--- frontend/src/controller/config.php
+++ frontend/src/controller/config.php
@@ -1,10 +1,11 @@
 <?php
-$db_host = 'db';
-$db_user = 'MYSQL_USER';
-$db_password = 'MYSQL_PASSWORD';
+$servername = 'db';
+$username = 'MYSQL_USER';
+$dbname = 'projet_bon_coin';
+$password = 'MYSQL_PASSWORD';
 
 try {
-    $conn = new PDO("mysql:host=$db_host;dbname=projet_bon_coin", $db_user, $db_password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
--- frontend/src/index.php
+++ frontend/src/index.php
@@ -28,20 +28,20 @@
     <div class="row">
         <form action="./controller/add_annonce.php" method="post">
             <div class="mb-3">
-            <?php
-                $host = 'db';
-                // Database use name
-                $user = 'MYSQL_USER';
-                //database user password
-                $pass = 'MYSQL_PASSWORD';
-                // check the MySQL connection status
-                $conn = new mysqli($host, $user, $pass);
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                } else {
-                    echo "Connected to MySQL server successfully!";
-                }
-            ?>
+                <?php
+                    $host = 'db';
+                    // Database use name
+                    $user = 'MYSQL_USER';
+                    //database user password
+                    $pass = 'MYSQL_PASSWORD';
+                    // check the MySQL connection status
+                    $conn = new mysqli($host, $user, $pass);
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    } else {
+                        echo "Connected to MySQL server successfully!";
+                    }
+                ?>
             </div>
         </form>
     </div>
