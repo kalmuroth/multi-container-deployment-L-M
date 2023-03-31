5e3375ed236a29a2b8c657768c23765615c94409
kalmuroth <dokkan.dracktar@gmail.com>
2023-03-31 15:00:12 +0200

    hostname/credential fixes

--- frontend/src/controller/config.php
+++ frontend/src/controller/config.php
@@ -1,11 +1,11 @@
 <?php
-$db_host = 'localhost';
+$db_host = '35.177.26.109';
 $db_user = 'root';
 $db_port = 3306;
-
+$db_password = 'MYSQL_ROOT_PASSWORD';
 
 try {
-    $conn = new PDO("mysql:host=$db_host;dbname=projet_bon_coin", $db_user);
+    $conn = new PDO("mysql:host=$db_host;dbname=projet_bon_coin", $db_user, $db_password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
