6faba69399f5a2b23af2a481bfb42587427c68f8
kalmuroth <dokkan.dracktar@gmail.com>
2023-04-01 04:47:12 +0200

    removed unit test

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -39,7 +39,7 @@
                     if ($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
                     } else {
-                        echo "Connected to MySQL server successfully!!!";
+                        echo "Connected to MySQL server successfully!!";
                     }
                 ?>
             </div>
