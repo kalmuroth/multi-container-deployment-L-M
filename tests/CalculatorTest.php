052e1c39230cd2f2e21e9e9f7d0fb0e92ebe9e3b
kalmuroth <dokkan.dracktar@gmail.com>
2023-04-01 04:21:19 +0200

    unit test

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+//require __DIR__ . "/calculator.php";
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+
+class CalculatorTest extends TestCase
+{
+   public function testAddOperation(): void
+   {
+       $adder = new Calculator("add");
+       $value = $adder->evaluate(2, 3);
+       $this->assertEquals($value, 5);
+   }
+
+   public function testSubtractOperation(): void
+   {
+       $adder = new Calculator("subtract");
+       $value = $adder->evaluate(2, 3);
+       $this->assertEquals($value, -1);
+   }
+}
